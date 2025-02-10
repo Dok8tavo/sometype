7472da69b7fa27a63726865ac9ec9b83faf7cac4
Dok8tavo <vladicxo.pasxo@protonmail.ch>
2025-02-10 05:56:21 +0100

    added `array_list.test.expect(.{ .allocator = true, .item_type = ..., .alignment = .at_least_natural })`

--- src/array_list.zig
+++ src/array_list.zig
@@ -662,7 +662,49 @@ test "expect(.{ .allocator = false, .alignment = .at_least_natural })" {
 }
 
 test "expect(.{ .allocator = true, .item_type = ..., .alignment = .at_least_natural })" {
-    // TODO
+    const Item = struct { [*:0]u8, bool };
+    const with = With{ .allocator = true, .item_type = Item, .alignment = .at_least_natural };
+
+    // passing
+    try expect(std.ArrayList(Item), with);
+    try expect(std.ArrayListAligned(Item, @alignOf(Item) * 2), with);
+
+    inline for (@typeInfo(Error).error_set.?) |error_info| {
+        const err: Error = @field(Error, error_info.name);
+        try expectError(switch (err) {
+            Error.NotAStruct => i32,
+            Error.IsTuple => struct { i32 },
+            Error.NoAllocator => std.ArrayListUnmanaged(u8),
+            Error.AllocatorNotAnAllocator => struct {
+                allocator: @TypeOf(.not_an_allocator),
+            },
+            Error.NoSlice => struct { allocator: std.mem.Allocator },
+            Error.SliceNotAType => struct {
+                allocator: std.mem.Allocator,
+                pub const Slice = "This is a string, not a type";
+            },
+            Error.SliceTypeNotAPointer => struct {
+                allocator: std.mem.Allocator,
+                pub const Slice = @TypeOf(.this_isnt_a_pointer);
+            },
+            Error.SliceTypeNotASlice => struct {
+                allocator: std.mem.Allocator,
+                pub const Slice = @TypeOf("This is a pointer, but not a slice");
+            },
+            Error.NotFromFunction => struct {
+                allocator: std.mem.Allocator,
+                pub const Slice = []Item;
+            },
+            Error.AlignmentTooSmall => std.ArrayListAligned(Item, @alignOf(Item) / 2),
+            // impossible to reach with `.alignment = .at_least_natural`
+            Error.AlignmentNotExact,
+            // impossible to reach with `.allocator = true`
+            Error.HasAllocator,
+            // impossible to reach with `.item_type = null`
+            Error.ItemNotItem,
+            => continue,
+        }, with, err);
+    }
 }
 
 test "expect(.{ .allocator = false, .item_type = ..., .alignment = .at_least_natural })" {
